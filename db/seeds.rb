5f964acb0d98ad04644c4aae659f0cf9737ba69d
MatthewGreenberg <mattcgreenberg@gmail.com>
2015-08-25 18:06:08 -0500

    style show page

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,7 +11,7 @@ require 'faker'
 
 matty = User.create!(is_admin: true, username: 'mattbaker', has_pro_permissions: false, password_digest: "1234")
 
-matty = User.create!(is_admin: true, name: "Matt Baker", username: 'mattbaker', has_pro_permissions: false, hashed_password: "1234")
+
 
 
 3.times do
