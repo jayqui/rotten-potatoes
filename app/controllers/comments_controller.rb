967162e3368bbe2392e179eed93dda2dc619e59f
Jay Quigley & Matthew Greenberg  & Josh Jones <pair+jayqui.matthewgreenberg.z3ntech@devbootcamp.com>
2015-08-26 10:08:38 -0500

    add staring to comments database & view partial

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -90,7 +90,7 @@ class CommentsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def comment_params
-      params.require(:comment).permit(:body)
+      params.require(:comment).permit(:body, :stars)
     end
 
     def commentable_of_comment
--- db/migrate/20150824190150_create_comments.rb
+++ db/migrate/20150824190150_create_comments.rb
@@ -1,6 +1,7 @@
 class CreateComments < ActiveRecord::Migration
   def change
     create_table :comments do |t|
+      t.integer :stars
       t.string :body
       t.references :commenter, null: false
       t.references :commentable, polymorphic: true, index: true, null: false
